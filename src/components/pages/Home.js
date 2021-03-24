869f6d14a9bd38b4ddbbaa040fb9817715bec057
eyaAbbassi <aabbassi91@yahoo.com>
2021-03-25 00:26:02 +0100

    eya commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../Footer/Footer';
+
+import Slide from '../Slide/Slide';
+
+
+function Home() {
+  return (
+    <>
+      <Slide />
+      <Footer/>
+    </>
+  );
+}
+
+export default Home;
