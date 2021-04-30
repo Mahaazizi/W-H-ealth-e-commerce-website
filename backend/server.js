419e735ccfc73c668f5d29fa0a31f2c967e7d16f
eyaAbbassi <aabbassi91@yahoo.com>
2021-04-30 03:29:20 +0100

    adding backend folder with node js and sending the data via json

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import data from './data.js';
+const app = express ();
+
+app.get ('/api/products',(req,res) => {
+ res.send(data.products);
+});
+
+app.get ('/',(req,res) => {
+ res.send('Server is ready');
+});
+
+app.listen(5000,()=>{
+ console.log('serve at http://localhost:5000')
+});
