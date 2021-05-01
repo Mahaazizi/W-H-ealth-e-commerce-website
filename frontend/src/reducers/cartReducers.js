af80cc82fe49e40ca117d68bd014ba6d558ffbe7
eyaAbbassi <aabbassi91@yahoo.com>
2021-05-01 15:37:56 +0100

    Implement Remove from Cart

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from '../constants/cartConstants';
+import { CART_ADD_ITEM , CART_REMOVE_ITEM} from '../constants/cartConstants';
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
@@ -7,14 +7,18 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
       const existItem = state.cartItems.find((x) => x.product === item.product);
       if (existItem) {
         return {
-          ...state,
-          cartItems: state.cartItems.map((x) =>
+          ...state, cartItems: state.cartItems.map((x) =>
             x.product === existItem.product ? item : x
           ),
         };
       } else {
         return { ...state, cartItems: [...state.cartItems, item] };
       }
+      case CART_REMOVE_ITEM:
+        return {
+          ...state,
+          cartItems: state.cartItems.filter((x) => x.product !== action.payload),
+        };
     default:
       return state;
   }
